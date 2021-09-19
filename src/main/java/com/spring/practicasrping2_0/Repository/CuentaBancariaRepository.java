62c9c7059de03d3f60d38d221a502187e56dc9c1
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-09-19 11:07:59 +0200

    added CuentaBancariaRepository

---
+++ src/main/java/com/spring/practicasrping2_0/Repository/CuentaBancariaRepository.java
@@ -0,0 +1,8 @@
+package com.spring.practicasrping2_0.Repository;
+
+import com.spring.practicasrping2_0.Entity.CuentaBancaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuentaBancariaRepository extends JpaRepository<CuentaBancaria, Long> {
+
+}
