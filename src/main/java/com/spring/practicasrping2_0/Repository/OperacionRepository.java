9a6982651787a9d858a57338c750f1f320a775fb
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-09-19 11:08:06 +0200

    added OperacionRepository

---
+++ src/main/java/com/spring/practicasrping2_0/Repository/OperacionRepository.java
@@ -0,0 +1,9 @@
+package com.spring.practicasrping2_0.Repository;
+
+
+import com.spring.practicasrping2_0.Entity.Operacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperacionRepository extends JpaRepository<Operacion, Long> {
+
+}
