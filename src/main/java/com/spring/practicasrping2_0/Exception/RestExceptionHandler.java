872ee137573ec728371c29cc2b556ecea9f21e47
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-09-25 11:23:17 +0200

    added hanndler client exceptions

---
+++ src/main/java/com/spring/practicasrping2_0/Exception/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.spring.practicasrping2_0.Exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.status;
+
+@RestControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+
+    @ExceptionHandler(value = {ClienteNotFoundException.class})
+    public ResponseEntity clientNotFound(ClienteNotFoundException ex, WebRequest request) {
+        log.debug("handling ClientNotFoundException...");
+        return notFound().build();
+    }
+
+ /*   @ExceptionHandler(value = {InvalidJwtAuthenticationException.class})
+    public ResponseEntity invalidJwtAuthentication(InvalidJwtAuthenticationException ex, WebRequest request) {
+        log.debug("handling InvalidJwtAuthenticationException...");
+        return status(UNAUTHORIZED).build();
+    }*/
+
+}
