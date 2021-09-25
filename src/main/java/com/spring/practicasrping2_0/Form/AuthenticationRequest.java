4fce3aba5ae28bdcf2ab807e60b34fa6198f77a7
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-09-25 12:04:43 +0200

    added autentication request class

---
+++ src/main/java/com/spring/practicasrping2_0/Form/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.spring.practicasrping2_0.Form;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+    private String username;
+    private String password;
+}
